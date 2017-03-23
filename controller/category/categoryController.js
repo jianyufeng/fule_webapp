ddc1822f8524a11d9cc87b9d33ff7cfc8a187816
lixufeng <44545545>
2017-03-23 17:43:03 +0800

    ShareLock

--- controller/category/categoryController.js
+++ controller/category/categoryController.js
@@ -1,21 +1,19 @@
-define(['app'],function(app){
+define(['app'], function (app) {
 
-	function ctrl($scope,categoryService){
+    function ctrl($scope, categoryService) {
 
-		console.log("分类界面控制器...");
+        console.log("分类界面控制器...");
 
-		console.log(categoryService);
-
-		$scope.$on('$ionicView.loaded', function() {
-			categoryService.getCategoryGoodsInfo($scope);
-		});
-
+        //console.log(categoryService);
+        $scope.$on('$ionicView.loaded', function () {
+            categoryService.getCategoryGoodsInfo($scope);
+        });
 
 
-	}
+    }
 
-	ctrl.$inject = ['$scope','categoryService'];
-	app.registerController('categoryController',ctrl);
+    ctrl.$inject = ['$scope', 'categoryService'];
+    app.registerController('categoryController', ctrl);
 
 
 });
