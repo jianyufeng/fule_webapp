ca2ca7e4c63ab2150aa98d189e8f3c17ea791ab4
Charles_xsx <547353243@qq.com>
2017-03-24 18:26:29 +0800

    购物车

--- controller/cart/cartController.js
+++ controller/cart/cartController.js
@@ -2,6 +2,11 @@ define(['app',"./Fun/cart_fun",'jquery_fly'],function(app,cart_fun){
 
 	function ctrl($scope){
 
+		$scope.aa = [];
+
+		$(".accountBox").click(function(){
+			alert(1);
+		});
 		
 		
 	}
