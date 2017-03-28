644932088f006dfff058720b3b34b67bc3c709f7
简玉锋 <jianyufeng@38.hn>
2017-03-28 09:04:35 +0800

    fsd

--- controller/login/loginController.js
+++ controller/login/loginController.js
@@ -9,6 +9,22 @@ define(['app',"css! ../../../css/login/login"],function(app){
             console.log("loginController")
 
         });
+
+
+        /*点击登录验证模式切换*/
+        $('.choiceModel').click(function (){
+            var  v = $(this).val();
+            if (v==0){
+                $(".phoneCheckBox").fadeIn(300);
+                $('.CardCheckBox').hide();
+            }else{
+                $(".phoneCheckBox").hide();
+                $('.CardCheckBox').fadeIn(300);
+
+            }
+        })
+
+
     }
 
     /*给构造函数添加$inject属性,添加注入的服务*/
