d6fd13e49051413f94d6a0474623a9e136a91cfd
叶静娅 <879872762@qq.com>
2017-03-31 18:07:14 +0800

    注册

--- register/registerController.js
+++ register/registerController.js
@@ -1,22 +1,97 @@
-//define(['registerApp'],function(app){
-//
-//    function ctrl($scope,$rootScope){
-//
-//        //.....
-//
-//
-//    }
-//
-//    ctrl.$inject = ['$scope','$rootScope'];
-//    app.registerController('registerController',ctrl);
-//
-//
-//});
+var app = angular.module("registerApp",['ionic']);
+
+app.controller("registerController", function ($scope, POP) {
+
+    /*点击发送短信效验码*/
+    $('.postNote').click(function () {
+        var user_name = $.trim($('#account').val());
+        //账号不为空
+        if (CommenFun.isNullObj(user_name)) {
+            POP.Hint("账号不能为空");
+            return;
+        }
+        var postNote = $('.postNote');
+        postNote.attr("disabled",true);
+        postNote.text("正在发送...");
+        //获取验证码
+        var url = "http://192.168.10.123:5000/_user/getSmsCode/user_name/"+user_name;
+
+        HTTP.get(url,{},function (e, data) {
+            if (e) {
+                POP.Hint("data");
+                postNote.removeAttr("disabled");
+                postNote.text("发送短信效验码");
+                return;
+            }
+            POP.Hint(data);
+            setTime(postNote);
+        });
+
+    });
+    var countdown = 8;
+    //定时60s
+    function setTime(obj) {
+        console.log(obj);
+        if (countdown == 0) {
+            obj.text("发送短信效验码");
+            countdown = 8;
+            obj.removeAttr("disabled");
+            return;
+        } else {
+            obj.attr("disabled", true);
+            obj.text("重新发送(" + countdown + ")");
+            countdown--;
+        }
+        setTimeout(function () {
+                setTime(obj)
+            }
+            , 1000)
+    }
 
 
 
-//var app = angular.module("loginApp", ['ionic']);
-//app.controller("loginController", function ($scope, POP) {
-//
-//
-//});
+
+    //注册
+    $('#register').click(function () {
+        var user_name = $.trim($('#account').val());
+        var email = $('#mailbox').val();
+        var loginPassword = $('#loginPassword').val();
+        var secondPassword = $('#secondPassword').val();
+        var threePassword = $('#threePassword').val();
+        var phoneNumber = $('#phone').val();
+        //获取手机验证码
+        var note = $('#note').val();
+        //手机验证码不为空
+        if (CommenFun.isNullObj(code)) {
+            POP.Hint("验证码不能为空");
+            return;
+        }
+        var codeCheck = note;
+        var verification_mode = "CODE";
+        var url = "http://192.168.10.123:5000/_user/register";
+        var param = {
+            'user_name':user_name,
+            'email':email,
+            'password':loginPassword,
+            'SECOND_PASSWORD':secondPassword,
+            'THREE_PASSWORD':threePassword,
+            'mobile':phoneNumber,
+            'verification_mode':verification_mode
+
+
+    };
+        param.code = codeCheck;
+        POP.StartLoading();
+
+        HTTP.post(url, param, function (e, data) {
+            POP.EndLoading();
+if (e) {
+    POP.Hint(data);
+    return;
+}
+            var userInfo = JSON.stringify(data);
+            console.log(userInfo);
+        })
+    });
+
+});
