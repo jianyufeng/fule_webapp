580eaabde91b93e581f11a7edf7996f76c939ac0
Apple <1124470898@qq.com>
2017-04-12 13:43:41 +0800

    更新

--- js/util.js
+++ js/util.js
@@ -78,6 +78,9 @@ define(function () {
 			timeout	: 150000,  //设置超时时间15秒
 			success	: function (data) {
 
+				console.log(111);
+				console.log(data);
+
 				//判断业务是否成功
 				if (data.success) {
 					_callback(null, data.result);
