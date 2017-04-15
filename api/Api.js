3a3a4b8df60df59f516331cb2d3dfc8b0cbaad8b
简玉锋 <jianyufeng@38.hn>
2017-04-15 17:23:56 +0800

    发送的

--- api/Api.js
+++ api/Api.js
@@ -47,6 +47,7 @@ var API = {
         // 一键升级到D+VIP+批发
         oneUpgrade: serverIP + "/upgrade/oneUpgrade",
         buyGoodsList:"http://umiddle.38zs.net:3101/happyHome/searchGoodsList/user_id/167642/user_name/app001",
+        buyGoodsMoreAttr:serverIP+"/goods/searchProductInfo",
         //获取喜乐之家配置表
         happyHomeList: "http://userm.38zs.net:3222/buyxlzj/happlyhomelist",
         //喜乐之家购买记录
