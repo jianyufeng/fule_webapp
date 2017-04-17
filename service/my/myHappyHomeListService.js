f590a8d73a8202954f3e67a651d6c1b6aa62b34f
Charles_xsx <547353243@qq.com>
2017-04-17 09:07:57 +0800

    认真

---
+++ service/my/myHappyHomeListService.js
@@ -0,0 +1,48 @@
+
+define(['app'],function(app){
+
+    app.factory("myHappyHomeListService",function(){
+
+        var service = {};
+
+        /* 获取服务器数据*/
+        /*网络获取用户信息*/
+        service.getHappyHomeList = function ($scope, POP) {
+
+
+            POP.StartLoading();
+
+            //获取用户的账号
+            var info = User.getInfo();
+            HTTP.get(API.My.happyHomeList, {}, function (e, data) {
+
+                POP.EndLoading();
+
+                  console.log(data);
+                if (e) {
+                    $.loadError(function () {
+                        service.getHappyHomeList();
+                    });
+                    return;
+                }
+
+
+                $scope.$apply(function () {
+                    //为html页面注入数据
+                    $scope.happyHomeData= data.data;
+
+                    console.log($scope.purchaseHistory);
+                });
+
+
+            });
+
+        };
+
+
+        return service;
+
+    });
+
+
+});
