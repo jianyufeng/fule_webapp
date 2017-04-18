830baeb812a7c8bb438acb44656858b4a2665793
Charles_xsx <547353243@qq.com>
2017-04-18 11:10:57 +0800

    no message

--- controller/my/Fun/my_fun.js
+++ controller/my/Fun/my_fun.js
@@ -48,5 +48,41 @@ define(['animate'],function(){
         });
     };
 
+
+    myFun.happyHomeSideslipping = function(isOpen){
+
+        if(isOpen){
+            $(".logsItems").animate({"left":-100},200);
+            $(".deleteBox").animate({"right":0},200);
+        }else{
+            $(".logsItems").animate({"left":0},200);
+            $(".deleteBox").animate({"right":-110},200);
+        }
+    }
+
+    myFun.happyHomeIdxSideslipping = function(isOpen,idx){
+        if(isOpen){
+            $(".logsItems:eq("+idx+")").animate({"left":-100},200);
+            $(".deleteBox:eq("+idx+")").animate({"right":0},200);
+        }else{
+            $(".logsItems:eq("+idx+")").animate({"left":0},200);
+            $(".deleteBox:eq("+idx+")").animate({"right":-110},200);
+        }
+    }
+
+
+    myFun.deleteHappyHomeBtn = function(fn){
+
+        $(document).on("click",".deleteBox",function(){
+            var _idx = $(".deleteBox").index(this);
+            var _id = $(this).attr("id");
+            // $(this).parent().slideUp(200,function(){
+            fn(_idx,_id);
+            // });
+        });
+    }
+
+
+
     return myFun;
 });
--- controller/my/myHappyHomeLogsController.js
+++ controller/my/myHappyHomeLogsController.js
@@ -1,6 +1,7 @@
 
 
-define(['app','css! ../../../css/my/my-happyHomeLogs'], function (app) {
+define(['app',"./Fun/my_fun",'css! ../../../css/my/my-happyHomeLogs'], function (app,my_fun) {
+
     function ctrl($scope,myHappyHomeLogsService,POP,$state) {
 
         $scope.$on('$ionicView.loaded', function () {
@@ -15,13 +16,50 @@ define(['app','css! ../../../css/my/my-happyHomeLogs'], function (app) {
 
 
 
-        //购买记录
-        $scope.editLogs  = function () {
+        //点击编辑购买记录
+        // var editOpen = false;
+        // $scope.editLogs = function(){
+        //
+        //     if(editOpen){
+        //         $scope.righttitleValue = "编辑";
+        //         my_fun.cartSideslipping(false);
+        //         editOpen = false;
+        //     }else{
+        //         $scope.righttitleValue = "关闭";
+        //         my_fun.cartSideslipping(true);
+        //         editOpen = true;
+        //
+        //
+        //     }
+        //
+        // };
 
-            alert("购买记录编辑");
-        };
+        //点击删除
+        // my_fun.deleteHappyHomeBtn(function(_idx,_id){
+        //
+        //     var info = User.getInfo();
+        //     var deleteParams = {
+        //         user_id : info.user_id,
+        //         shopping_type : 1,
+        //         id : _id
+        //     }
 
 
+            //删除购物车
+
+            // myHappyHomeLogsService.deleteCart($scope,deleteParams,POP,_idx,$rootScope);
+
+
+        // })
+
+        // //左滑动
+        // $scope.swipLeft = function(idx,id){
+        //     my_fun.happyHomeIdxSideslipping(true,idx);
+        // }
+        //
+        // $scope.swipRight = function(idx,id){
+        //     my_fun.happyHomeIdxSideslipping(false,idx);
+        // };
 
 
 
