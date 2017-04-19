1a6176ac9b2356efbd97fdad02a5f1ea68d84ff6
简玉锋 <jianyufeng@38.hn>
2017-04-19 14:30:46 +0800

    购买

--- api/Api.js
+++ api/Api.js
@@ -49,6 +49,8 @@ var API = {
         //购买喜乐之家列表
         buyGoodsList:serverIP+  "/_happyHome/searchGoodsList/user_id/167642/user_name/app001",
         buyGoodsMoreAttr:serverIP+"/goods/searchProductInfo",
+        //获取购买喜乐之家配置
+        buyHappyHomeGoodsConfig:serverIP+"/buyxlzj/happlyhomelist",
         //获取喜乐之家配置表
         happyHomeList: "http://userm.38zs.net:3222/buyxlzj/happlyhomelist",
         //喜乐之家购买记录
--- service/my/myBuyHappyHomeServer.js
+++ service/my/myBuyHappyHomeServer.js
@@ -14,10 +14,23 @@ define(['app'], function (app) {
                         POP.Hint("加载失败");
                         return;
                     }
-                    first = false;
-                    console.log(data);
                     $scope.$apply(function () {
                         $scope.goods = data.goodsInfo.data;
+                    })
+                }
+            );
+        };
+        /*喜乐之家商品列表 信息*/
+        service.getBuyGoodConfig = function ($scope, POP,id) {
+            //获取用户的账号
+            HTTP.get(API.My.buyHappyHomeGoodsConfig +"/id/"+id, {}, function (e, data) {
+                    if (e) {
+                        POP.Hint("加载失败");
+                        return;
+                    }
+                    console.log(data);
+                    $scope.$apply(function () {
+                        $scope.bugConfig = data.data[0];
                     })
                 }
             );
@@ -38,41 +51,41 @@ define(['app'], function (app) {
 
                         var length = data.length;
                         for (var i = 0; i < length; i++) {
+
+                            var tempHtml = "";
+
+                            var lth = data[i].attr_info.length;
+                            for (var j = 0; j < lth; j++) {
+                                var attrInfo = data[i].attr_info[j];
+                                var tem = [
+                                    '<div class="goodAttrNameBox">',
+                                    attrInfo.attr_name + '&nbsp;:&nbsp;',
+                                    '<span class="goodAttrVal">' + attrInfo.attr_value + '</span>',
+                                    '</div>'
+                                ].join("");
+                                tempHtml += tem;
+                            }
+                            var moreGoodNameObj = "<div class='more_goodName'>" + tempHtml + "</div>";
+
                             var template = [
                                 '<div class="more_goodsBox">',
                                 '   <div class="more_goodImgBox">',
-                                '       <div class="more_goodName" id="more_goodName_' + goodId + '' + i + ' ">',
-                                '       </div>',
+                                moreGoodNameObj,
                                 '    </div>',
                                 '    <div class="more_goodInfo">',
                                 '              <div class="bbh_goodPrice">单价&nbsp;:&nbsp;255</div>',
                                 '               <div class="bbh_goodMoney">实际金额&nbsp;:&nbsp;0</div>',
                                 '     </div>',
                                 '     <div class="more_buyNumberBox">',
+                                '           <div class="more_noGoods">商品库存不足</div>',
                                 '           <input class="bhh_buyNumber" type="number" placeholder="购买数量">',
                                 '     </div>',
                                 '     <div style="clear: both"> ',
                                 '     </div>',
                                 '</div>'
                             ].join("");
-                            var modelHtml = $compile(template)($scope);
-                            $("#more_goodsBox_" + goodId).append(modelHtml);
-
-
-                            var lth =  data[i].attr_info.length
-                            for(var j= 0;j<lth;j++){
-                                var tem = [
-                                    '<div class="goodAttrNameBox">',
-                                    '   属性&nbsp;:&nbsp;',
-                                    '   <span class="goodAttrVal">值</span>',
-                                    '</div>'
-                                ].join("");
-                                var modelH = $compile(tem)($scope);
-                                var sss = "#more_goodName_" + goodId + i ;
-                                console.log(sss)
-                                console.log($('#sss'));
-                                $("#more_goodName_" + goodId + i).append(modelH);
-                            }
+                            // var modelHtml = $compile(template)($scope);
+                            $("#more_goodsBox_" + goodId).append(template);
                         }
 
 
