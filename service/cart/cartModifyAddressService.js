75ac3763f68c602f965270ccef192e03e70bf6de
Charles_xsx <547353243@qq.com>
2017-04-21 14:16:13 +0800

    喜乐之家地址UI修正

--- service/cart/cartModifyAddressService.js
+++ service/cart/cartModifyAddressService.js
@@ -1,9 +1,4 @@
-/**
- * Created by charles_xsx on 2017/4/8.
- */
-/**
- * Created by charles_xsx on 2017/3/30.
- */
+
 define(['app'],function(app){
 
     app.factory("cartModifyAddressService",function(){
@@ -13,18 +8,14 @@ define(['app'],function(app){
         /*设置默认收货地址*/
         service.setModifyAddress = function($scope,updateParams,POP,fn){
 
-            console.log(111111111111);
-            console.log(updateParams);
             //var p = updateParams;
       
-            // POP.StartLoading();
+            POP.StartLoading();
 
             //更新操作
             HTTP.post(API.Cart.updateUserAddress,updateParams,function(e,data){
 
                 POP.EndLoading();
-                console.log("编辑地址错误" + e);
-                console.log("编辑地址" + data);
                 if(e){
                     POP.Hint("设置失败");
                     return;
