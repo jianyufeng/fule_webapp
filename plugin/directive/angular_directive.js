07e466d778b8cd34c9291f53d49304202bd2fb82
Apple <1124470898@qq.com>
2017-05-02 13:45:01 +0800

    更新

--- plugin/directive/angular_directive.js
+++ plugin/directive/angular_directive.js
@@ -89,19 +89,27 @@ define(['app'], function (app) {
                 title: '@',
                 isBack: '@',
                 righttitle: '@',
+                isright: '@',
                 ishint: '@',
                 isOverBack: '@',
-                onRightClick: "&onRightClick",
+                onRightClick: "&onRightClick", 
                 onOverBackClick: "&onOverBackClick"
             },
             template: [
                 '<div class="headerBox" style="z-index: 99999">',
                 '<div class="leftHeaderBox" ng-click="backBtn()"><img ng-show="{{isBack}}"  src="./resource/images/icon/jiantou_left.png" style="width:11px;height:auto;" /></div>',
                 '<div class="middleHeaderBox">{{title}}</div>',
-                '<div class="rightHeaderBox" ng-click="rightIconClick()" ng-show="{{righttitle}}">{{righttitle}}<div class="iconHint" ng-show="{{ishint}}"></div></div>',
+                '<div class="rightHeaderBox" ng-click="rightIconClick()">{{righttitle}}<div class="iconHint" ng-show="{{ishint}}"></div></div>',
                 '</div>'
             ].join(""),
             link: function (scope, element, attrs) {
+
+                if(scope.isright == 'yes'){
+                    $(".rightHeaderBox").show();
+                }else{
+                    $(".rightHeaderBox").hide();
+                }
+
                 scope.rightIconClick = function () {
                     scope.onRightClick();
                 };
