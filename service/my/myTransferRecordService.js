52cf6da458f387838d8b4e212781996d24a60c05
简玉锋 <jianyufeng@38.hn>
2017-05-02 18:06:44 +0800

    登录

--- service/my/myTransferRecordService.js
+++ service/my/myTransferRecordService.js
@@ -17,14 +17,6 @@ define(['app'], function (app) {
                     POP.Hint("加载失败");
                     return;
                 }
-                //如果是上拉则添加到上次数据的后面
-                $scope.$apply(function(){
-                    if ($scope.isCanPull) {
-                        $scope.data = $scope.data.concat(data.data);
-                    } else {
-                        $scope.data = data.data;
-                    }
-                });
                 $scope.$apply(function () {
                     //根据是否上拉不同处理
                     var length = data.data.length;
@@ -55,31 +47,31 @@ define(['app'], function (app) {
         };
 
         /*网络获取转账记录 信息*/
-        service.getVouTransferRecord = function ($scope, POP) {
-            //if ($scope.isCanPull) {
-            //    POP.StartLoading();
-            //}
+        service.getVouTransferRecord = function ($scope, POP, isCanPull) {
+            if (isCanPull) {
+                POP.StartLoading();
+            }
 
             //获取用户的账号
             var info = User.getInfo();
             HTTP.get(API.My.voucherTransferRecord + "/skip/" + $scope.page * 10 + "/limit/10" + "/user_name/" + info.user_name, {}, function (e, data) {
-                //POP.EndLoading();
-               if (e) {
+                POP.EndLoading();
+                if (e) {
                     POP.Hint("加载失败");
                     return;
                 }
+                var length = data.data.length;
                 //如果是上拉则添加到上次数据的后面
                 if ($scope.isCanPull) {
                     $scope.data = $scope.data.concat(data.data);
                 } else {
                     $scope.data = data.data;
-                }
-                //判断数据是否为空
-                var length = data.data.length;
-                if (length <= 0) {
-                    $scope.isEmptyData = true;
-                } else {
-                    $scope.isEmptyData = false;
+                    //判断数据是否为空
+                    if (length <= 0) {
+                        $scope.isEmptyData = true;
+                    } else {
+                        $scope.isEmptyData = false;
+                    }
                 }
                 //判断是否有下页数据
                 if (length < 10) {
