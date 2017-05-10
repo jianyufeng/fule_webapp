40be0cc904a975838aaefd57b432f59f76f95c1f
简玉锋 <jianyufeng@38.hn>
2017-05-10 15:34:16 +0800

    确认检查 部分提取 和更改

--- controller/category/categoryController.js
+++ controller/category/categoryController.js
@@ -45,11 +45,19 @@ define(['app', "./Fun/caregoryFun"], function (app, caregoryFun) {
         $scope.$on("viewOnFinish", function () {
 
 			$(".goodsImg img").myImageLazyLoad({
-				//默认三个参数可不传，使用默认参数
-				// imageLoadErr : "./resource/images/default/default_image.png", //加载失败占位图
-				// imageServer : "http://image.38zs.net:848",				    //图片服务器地址
-				// animate     : true,											//是否动画显示
-			});
+                //默认三个参数可不传，使用默认参数
+                // imageLoadErr : "./resource/images/default/default_image.png", //加载失败占位图
+                // imageServer : "http://image.38zs.net:848",				    //图片服务器地址
+                // animate     : true,											//是否动画显示
+            });
+
+
+            $(".categoryImageBox img").myImageLazyLoad({
+                //默认三个参数可不传，使用默认参数
+                // imageLoadErr : "./resource/images/default/default_image.png", //加载失败占位图
+                // imageServer : "http://image.38zs.net:848",				    //图片服务器地址
+                // animate     : true,											//是否动画显示
+            });
 
          
         });
