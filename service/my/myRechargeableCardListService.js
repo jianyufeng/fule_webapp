1736f4630d684ae344252af43d9e8c2aa4eefce7
lixufeng <44545545>
2017-05-18 17:34:49 +0800

    sd

--- service/my/myRechargeableCardListService.js
+++ service/my/myRechargeableCardListService.js
@@ -19,24 +19,40 @@ define(['app'], function (app) {
                     return;
                 }
                 $scope.$apply(function () {
+                    // 电子币
                     var listA = [];
+                    // 辅销币
                     var listB = [];
+                    // 辅销币积分
+                    var listC = [];
                     for (var i = 0; i < data.data.length; i++) {
                         var item = data.data[i];
-                        if (item.user_money > 0 && item.fxp_points <= 0) {
+                        if (item.user_money > 0) {
                             listA.push(item);
                         }
-                        if (item.fxp_points > 0 && item.user_money <= 0) {
+
+                        if (item.fxp_money > 0) {
                             listB.push(item);
+                        }
+
+                        if (item.fxp_points > 0) {
+                            listC.push(item);
                         }
                     }
 
                     $scope.rechargeableCardAList = listA;
                     $scope.rechargeableCardBList = listB;
-                    if(data.data.length<=0){
-                        $scope.isEmptyData=true;
-                    }else{
-                        $scope.isEmptyData=false;
+                    $scope.rechargeableCardCList = listC;
+
+                    console.log(listA.length);
+                    console.log(listB.length);
+                    console.log(listC.length);
+
+
+                    if (data.data.length <= 0) {
+                        $scope.isEmptyData = true;
+                    } else {
+                        $scope.isEmptyData = false;
                     }
 
                 });
