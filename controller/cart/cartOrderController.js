fae84aad83b5c2b46a10bce830e441d55c1e9519
Charles_xsx <547353243@qq.com>
2017-07-08 15:27:44 +0800

    喜乐会所

--- controller/cart/cartOrderController.js
+++ controller/cart/cartOrderController.js
@@ -154,7 +154,7 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
 
 
                 console.log($scope.xlhs_selected);
-                if($scope.xlhs_selected){
+                if($scope.xlhs_selected && $scope.xlhs_selected != ""){
 
                     orderParams = {
                         user_id: info.user_id, //用户id
@@ -341,8 +341,39 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
 
         });
 
+
+        //解决第三方软键盘唤起时底部input输入框被遮挡问题
+        var bfscrolltop = document.body.scrollTop;//获取软键盘唤起前浏览器滚动部分的高度
+        $(".inputUser").focus(function(){//在这里‘input.inputframe’是我的底部输入栏的输入框，当它获取焦点时触发事件
+            interval = setInterval(function(){//设置一个计时器，时间设置与软键盘弹出所需时间相近
+                document.body.scrollTop = document.body.scrollHeight;//获取焦点后将浏览器内所有内容高度赋给浏览器滚动部分高度
+            },100)
+        }).blur(function(){//设定输入框失去焦点时的事件
+            clearInterval(interval);//清除计时器
+            document.body.scrollTop = bfscrolltop;//将软键盘唤起前的浏览器滚动部分高度重新赋给改变后的高度
+        });
+
         //弹出发货地址选择框()
         $(".orderSendGoodsAddress").click(function () {
+
+
+            $(".popBg").fadeIn(300);
+
+            var $popAddressBox = $(".popAddressBox");
+
+            $popAddressBox.css({
+                display: "block"
+            });
+            $popAddressBox.animate({"bottom": 0}, 300);
+
+
+            $(".close").click(function () {
+                $(".popBg").fadeOut(200);
+                $popAddressBox.animate({"bottom": -1000}, 200);
+
+            });
+
+
 
             $(".inputUser").val("");
             var exclusiveShopParams;
@@ -354,9 +385,6 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
                 return;
             }
 
-            console.log("就是这");
-            console.log($scope.isNullUser_name);
-            console.log($scope.xlhs_selected);
 
             if($scope.isNullUser_name == "NO" || $scope.xlhs_selected == null || $scope.xlhs_selected == undefined ||$scope.xlhs_selected == ""){
 
@@ -393,28 +421,13 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
 
             }
 
-            $(".popBg").fadeIn(300);
 
-            var $popAddressBox = $(".popAddressBox");
-
-            $popAddressBox.css({
-                display: "block"
-            });
-            $popAddressBox.animate({"bottom": 0}, 300);
-
-
-            $(".close").click(function () {
-                $(".popBg").fadeOut(200);
-                $popAddressBox.animate({"bottom": -1000}, 200);
-
-            });
 
         });
 
         $scope.aaa = function(idx,event){
 
 
-            //alert($(event.target));
             var obj = $(event.target);
 
             $(".choice").find("img").hide();
@@ -426,7 +439,7 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
             }else{
                 obj.find("img").hide();
                 obj.css("border", "1px solid #d79ac4");
-                $scope.xlhs_selected = null;
+                $scope.xlhs_selected = "";
             }
 
         };
