4e7d66c8b8a34b749fb2c7685bd95e7f83934314
Charles_xsx <547353243@qq.com>
2017-07-10 18:10:26 +0800

    方法

--- controller/cart/cartOrderController.js
+++ controller/cart/cartOrderController.js
@@ -342,6 +342,8 @@ define(['app', 'css!../../../css/cart/cart_orderConfirm'], function (app) {
         });
 
 
+        
+
         //解决第三方软键盘唤起时底部input输入框被遮挡问题
         var bfscrolltop = document.body.scrollTop;//获取软键盘唤起前浏览器滚动部分的高度
         $(".inputUser").focus(function(){//在这里‘input.inputframe’是我的底部输入栏的输入框，当它获取焦点时触发事件
