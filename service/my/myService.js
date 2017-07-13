0cc043c4dd78aedf4e2c851b70f5fbeb6309001c
lixufeng <44545545>
2017-07-13 09:29:20 +0800

    123

--- service/my/myService.js
+++ service/my/myService.js
@@ -23,10 +23,10 @@ define(['app'], function (app) {
                     return;
                 }
                 //获取用户图像
-                if(data.userInfo.headerUrl != undefined && data.userInfo.headerUrl != null && data.userInfo.headerUrl.length >4){
-                    $("#usrIcon").attr('src',"http://image.38zs.net:38888" + data.userInfo.headerUrl);
-                }else {
-                    $("#usrIcon").attr('src',"./resource/images/default/morentouxiang.jpg");
+                if (data.userInfo.headerUrl != undefined && data.userInfo.headerUrl != null && data.userInfo.headerUrl.length > 4) {
+                    $("#usrIcon").attr('src', "http://image.38zs.net:38888" + data.userInfo.headerUrl);
+                } else {
+                    $("#usrIcon").attr('src', "./resource/images/default/morentouxiang.jpg");
                 }
                 // D级别的标准
                 var IS_D = data.config.IS_D;
@@ -36,6 +36,8 @@ define(['app'], function (app) {
                 var IS_PI_FA = data.config.IS_PI_FA;
                 // 高级志愿者标准
                 var GAOJI_ZHI_YUAN_ZHE = data.config.GAOJI_ZHI_YUAN_ZHE;
+                // 共享圈升级积分标准
+                var IS_REAL_SHARE = data.config.IS_REAL_SHARE;
                 // 当前用户的级别
                 var lv = data.userInfo.REGISTER_GRADE;
                 $scope.registerGrade = lv;
@@ -45,6 +47,12 @@ define(['app'], function (app) {
                 var user_IS_PI_FA = data.userInfo.IS_PI_FA;
                 // 升级积分
                 var user_INTEGRAL = data.userInfo.INTEGRAL;
+                //  共享圈/事业圈
+                var share_type = data.userInfo.USER_SHARE_TYPE;
+                // 激活时间
+                var share_time = data.userInfo.USER_SHARE_TIME;
+
+
                 $scope.$apply(function () {
                     $scope.userInfo = data.userInfo;
                     $scope.travel_points = data.travel_points;
@@ -69,42 +77,94 @@ define(['app'], function (app) {
                         $('.isNotRegister').show();
                         $('#emptyFull').hide();  //
                     }
-                    if (lv < 4) {
-                        // 判断一键升级
-                        if (user_INTEGRAL >= GAOJI_ZHI_YUAN_ZHE) {
-                            // 显示一键升级
-                            $("#gradeButton").text("一键升级");
+
+                    //  事业圈
+                    if (share_type == 0) {
+                        // 判断 REGISTER_GRADE
+                        if (lv < 4) {
+                            // 判断一键升级
+                            $(".levelName").text("事业圈·注册会员");
+                            if (user_INTEGRAL >= GAOJI_ZHI_YUAN_ZHE) {
+                                // 显示一键升级
+                                $("#gradeButton").text("一键升级");
+                                $("#gradeButton").css("display", "block");
+                                $scope.garde = 1;
+                            } else if (user_INTEGRAL >= IS_D && user_INTEGRAL < GAOJI_ZHI_YUAN_ZHE) {
+                                // 显示升级D级
+                                $("#gradeButton").text("升级为志愿者");
+                                $("#gradeButton").css("display", "block");
+                                $scope.garde = 2;
+                            } else {
+                                $("#gradeButton").css("display", "none");
+                            }
+
+                        } else if (lv = 4 && user_IS_VIP == 0 && user_INTEGRAL >= IS_VIP) {
+                            // 显示升级VIP
+                            $("#gradeButton").text("升级为VIP");
                             $("#gradeButton").css("display", "block");
-                            $scope.garde = 1;
-                        } else if (user_INTEGRAL >= IS_D && user_INTEGRAL < GAOJI_ZHI_YUAN_ZHE) {
-                            // 显示升级D级
-                            $("#gradeButton").text("升级为志愿者");
+                            $(".levelName").text("事业圈·志愿者D");
+                            $scope.garde = 3;
+                        } else if (lv = 4 && user_IS_VIP == 1 && user_IS_PI_FA == 0 && user_INTEGRAL >= IS_PI_FA) {
+                            //  显示升级批发
+                            $("#gradeButton").text("升级为批发");
                             $("#gradeButton").css("display", "block");
-                            $scope.garde = 2;
+                            $(".levelName").text("事业圈·VIP");
+                            $scope.garde = 4;
                         } else {
                             $("#gradeButton").css("display", "none");
                         }
 
-                    } else if (lv = 4 && user_IS_VIP == 0 && user_INTEGRAL >= IS_VIP) {
-                        // 显示升级VIP
-                        $("#gradeButton").text("升级为VIP");
-                        $("#gradeButton").css("display", "block");
-                        $scope.garde = 3;
-                    } else if (lv = 4 && user_IS_VIP == 1 && user_IS_PI_FA == 0 && user_INTEGRAL >= IS_PI_FA) {
-                        //  显示升级批发
-                        $("#gradeButton").text("升级为批发");
-                        $("#gradeButton").css("display", "block");
-                        $scope.garde = 4;
-                    } else {
-                        $("#gradeButton").css("display", "none");
+                    } else
+                    //  共享圈
+                    if (share_type == 1) {
+                        if (share_time > 0) {
+                            $(".levelName").text("共享圈·激活会员");
+                        } else {
+                            $(".levelName").text("共享圈·注册会员");
+                        }
+                        if (lv < 4) {
+                            // 判断一键升级
+                            if (user_INTEGRAL >= GAOJI_ZHI_YUAN_ZHE) {
+                                // 显示一键升级
+                                $("#gradeButton").text("一键升级");
+                                $("#gradeButton").css("display", "block");
+                                $scope.garde = 1;
+                            } else if (user_INTEGRAL >= IS_D && user_INTEGRAL < GAOJI_ZHI_YUAN_ZHE) {
+                                // 显示升级D级
+                                $("#gradeButton").text("升级为志愿者");
+                                $("#gradeButton").css("display", "block");
+                                $scope.garde = 2;
+                            } else if (user_INTEGRAL < IS_D && user_INTEGRAL > IS_REAL_SHARE) {
+                                // 升级为共享圈注册会员
+                                // 显示升级D级
+                                $("#gradeButton").text("激活共享圈");
+                                $("#gradeButton").css("display", "block");
+                                $scope.garde = 5;
+                            } else {
+                                $("#gradeButton").css("display", "none");
+                                console.log(55555555555);
+                                console.log($scope.garde)
+                            }
+
+                        } else if (lv = 4 && user_IS_VIP == 0 && user_INTEGRAL >= IS_VIP) {
+                            // 显示升级VIP
+                            $("#gradeButton").text("升级为VIP");
+                            $("#gradeButton").css("display", "block");
+                            $scope.garde = 3;
+                            $(".levelName").text("共享圈·志愿者D");
+                        } else if (lv = 4 && user_IS_VIP == 1 && user_IS_PI_FA == 0 && user_INTEGRAL >= IS_PI_FA) {
+                            //  显示升级批发
+                            $("#gradeButton").text("升级为批发");
+                            $("#gradeButton").css("display", "block");
+                            $(".levelName").text("共享圈·VIP");
+                            $scope.garde = 4;
+                        } else {
+                            $("#gradeButton").css("display", "none");
+                        }
                     }
-
-
                     if (!isRefresh) {
                         $.initAppEndLoad();
                     }
-
-
                 });
                 if (isRefresh) {
                     $scope.$broadcast('scroll.refreshComplete');
